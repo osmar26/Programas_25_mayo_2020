6d87b7a32b194969ca943a3bcd881dddcc921536
AnaBelemmm <60456648+AnaBelemmm@users.noreply.github.com>
2020-05-26 20:59:40 -0500

    Add files via upload

---
+++ Programa 2/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+extern int yylex();
+extern FILE *yyin;
+
+int main (int argc, char** argv){
+    FILE *f;
+    if(argc<2){
+        printf("Faltan argumentos\n");
+        return -1;
+    }
+    f = fopen(argv[1], "r");
+    if(!f){
+        printf("El archivo %s no se puede abrir\n", argv[1]);
+        return -1;
+    }
+
+    yyin = f;
+    int tok = yylex();
+    while(tok != 0){
+        printf("%d\n",tok);
+        tok = yylex();
+    }  
+    fclose(yyin);
+    return 0;
+}
